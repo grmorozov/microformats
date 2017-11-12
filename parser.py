7383b7dc73755bb9199fe9c6985c38b16c35188a
uchenic <uchenic@ubuntu.home>
2017-11-11 20:03:08 -0800

    - add basic value-class-pattern parsing - working on p-property parsing

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import json
 from bs4 import BeautifulSoup, Tag, NavigableString
-# from typing import List, Dict, Optional
+from typing import List, Dict, Optional
 
 
 def parse(html=None, path=None, url=None) -> dict:
@@ -65,64 +65,84 @@ class Parser:
     def parse_tag(self, tag: Tag) -> dict:
         properties = {}
         if tag.has_attr('class'):
-            for c in tag['class']:
-                class_name = c[2:]
-                if c.startswith('p-'):
-                    p = self.parse_p_property(tag, class_name=class_name)
-                    properties = {**properties, **p}
-                elif c.startswith('u-'):
-                    u = self.parse_u_property(tag)
-                    properties = {**properties, **u}
-                elif c.startswith('dt-'):
-                    dt = self.parse_dt_property(tag)
-                    properties = {**properties, **dt}
-                elif c.startswith('e-'):
-                    e = self.parse_e_property(tag)
-                    properties = {**properties, **e}
-
+            if any([c for c in tag['class'] if c.startswith('p-')]):
+                self.parse_p_property(tag, properties)
+            if any([c for c in tag['class'] if c.startswith('u-')]):
+                self.parse_u_property(tag, properties)
+            if any([c for c in tag['class'] if c.startswith('dt-')]):
+                self.parse_dt_property(tag, properties)
+            if any([c for c in tag['class'] if c.startswith('e-')]):
+                self.parse_e_property(tag, properties)
+        # todo: support nesting
+        # if one of special properties was found
         if len(properties.items()) == 0:
             # todo: add recursive search for h- classes
             pass
 
         return properties
 
-    def parse_p_property(self, tag: Tag, class_name: str) -> dict:
-        properties = {}
-        # todo: value-class-pattern
+    def parse_p_property(self, tag: Tag, properties: dict):
+        class_names = [c[2:] for c in tag['class'] if c.startswith('p-')]
 
-        # todo: support nesting
-        if any([c for c in tag['class'] if c.startswith('h-')]):
-            properties[class_name] = [self.parse_h_tag(tag)]
+        if self.is_value_class(tag):
+            value = self.parse_value_class(tag)
+        elif any([c for c in tag['class'] if c.startswith('h-')]):
+            value = self.parse_h_tag(tag)
         elif tag.name == 'abbr' and tag.has_attr('title'):
-            properties[class_name] = tag['title']
+            value = tag['title']
         elif tag.name in ('data', 'input') and tag.has_attr('value'):
-            properties[class_name] = tag['value']
+            value = tag['value']
         elif tag.name in ('img', 'area') and tag.has_attr('alt'):
-            properties[class_name] = tag['alt']
+            value = tag['alt']
         else:
             # todo: replace any nested <img> elements with their alt attribute, if present;
             # otherwise their src attribute, if present, adding a space at the beginning and end,
             # resolving any relative URLs, and removing all leading/trailing whitespace.
-            properties[class_name] = tag.get_text()
-        return properties
+            value = tag.text
+        if value:
+            for name in class_names:
+                if properties.get(name) is not None:
+                    properties[name].append(value)
+                else:
+                    properties[name] = [value]
 
-    def parse_u_property(self, tag: Tag) -> dict:
+    def parse_u_property(self, tag: Tag, properties: dict) -> dict:
         return {}
 
-    def parse_dt_property(self, tag: Tag) -> dict:
+    def parse_dt_property(self, tag: Tag, properties: dict) -> dict:
         return {}
 
-    def parse_e_property(self, tag: Tag) -> dict:
+    def parse_e_property(self, tag: Tag, properties: dict) -> dict:
         return {}
 
-    def parse_h_property(self, tag: Tag) -> dict:
-        return {}
+    def parse_value_class(self, tag: Tag) -> str:
+        value_tags = [child for child in tag.findChildren(attrs={'class': 'value'}, recursive=False)]
+        # todo: implement special rules for date-time parsing
+        values = [self.parse_value_tag(vt) for vt in value_tags]
+        result = ''.join(values)
+        return result
+
+    @staticmethod
+    def is_value_class(tag: Tag) -> bool:
+        return any([child for child in tag.findChildren(attrs={'class': 'value'}, recursive=False)])
+
+    @staticmethod
+    def parse_value_tag(tag: Tag) -> str:
+        # todo: check if tag.text is equal to innerText
+        if tag.name in ('img', 'area') and tag.has_attr('alt'):
+            return tag['alt']
+        if tag.name == 'data':
+            return tag['value'] if tag.has_attr('value') else tag.text
+        if tag.name == 'abbr':
+            return tag['title'] if tag.has_attr('title') else tag.text
+        return tag.text
 
 
 test_html = '<html>' \
        '<title>test example</title>' \
        '<body>' \
-       '<div class="h-card">Grey wolf</div>' \
+       '<div class="h-card"><span class="p-name p-fullname">Test name</span></div>' \
+            '<div class="h-card">Grey wolf</div>' \
        '<div class="h-adr">London</div>' \
        '<div class="h-card">' \
        '    <a class="p-name u-url" href="http://blog.lizardwrangler.com/">Mitchell Baker</a>' \
--- tests/test_p_property.py
+++ tests/test_p_property.py
@@ -5,18 +5,39 @@ def test_simplest_p_property():
     html = '<div class="h-card"><span class="p-name">Test name</span></div>'
     parser = Parser()
     result_dict = parser.parse(html)
-    assert result_dict['items'][0]['properties']['name'] == 'Test name'
+    assert result_dict['items'][0]['properties']['name'] == ['Test name']
 
 
 def test_abbr_p_property():
     html = '<div class="h-card"><abbr class="p-additional-name" title="Peter">P</abbr></div>'
     parser = Parser()
     result_dict = parser.parse(html)
-    assert result_dict['items'][0]['properties']['additional-name'] == 'Peter'
+    assert result_dict['items'][0]['properties']['additional-name'] == ['Peter']
 
 
 def test_abbr_p_property_recursive():
     html = '<div class="h-card"><span class="p-name"><abbr class="p-additional-name" title="Jon">J</abbr></span></div>'
     parser = Parser()
     result_dict = parser.parse(html)
-    assert result_dict['items'][0]['properties']['additional-name'] == 'Jon'
+    assert result_dict['items'][0]['properties']['additional-name'] == ['Jon']
+
+
+def test_multiple_p_property_name():
+    html = '<div class="h-card"><span class="p-name p-fullname">Test name</span></div>'
+    parser = Parser()
+    result_dict = parser.parse(html)
+    assert result_dict['items'][0]['properties']['name'] == ['Test name']
+    assert result_dict['items'][0]['properties']['fullname'] == ['Test name']
+
+
+def test_value_class_pattern():
+    html = '<div class="h-card">' \
+           '<span class="p-name">' \
+           '<span class="p-given-name value">John</span>' \
+           '<abbr class="p-additional-name" title="Peter">P</abbr>' \
+           '<span class="p-family-name value ">Doe</span>' \
+           '</span></div>'
+    parser = Parser()
+    result_dict = parser.parse(html)
+    assert result_dict['items'][0]['properties']['name'] == ['JohnDoe']
+
